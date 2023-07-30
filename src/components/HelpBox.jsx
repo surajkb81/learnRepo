b7408fe22e31f38d30e5430be0e11703b9ee94ab
surajkb <suraj_kb@hotmail.com>
2023-07-30 10:40:15 +0530

    updated issue

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -5,7 +5,7 @@ import './HelpBox.css';
 function HelpBox({ title, text }) {
   return (
     <article className="help-box">
-      <h1>{title}</h1>
+      <h2>{title}</h2>
       <p>{text}</p>
     </article>
   );
